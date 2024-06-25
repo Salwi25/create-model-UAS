cdf10d2120ef068916a0836132e9d3a24ef40e60
itsweape <rengganis.wahyuaning.putri@mail.ugm.ac.id>
2024-06-25 23:15:15 +0700

    feat: update model

--- breast_cancer_predict.py
+++ breast_cancer_predict.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 2020-11-18
-@author: Dr. Ganjar Alfian
-email : ganjar@dongguk.edu
-for teaching purpose only.
-"""
-
 import pandas as pd
 import joblib
     
@@ -14,13 +6,13 @@ if __name__ == '__main__':
     filename = 'lr_joblib.model'
     loaded_model= joblib.load(filename)
     #create new unlabelled test data
-    df_input = pd.DataFrame(columns = ["diagnosis","radius_mean","texture_mean","perimeter_mean","area_mean",
+    df_input = pd.DataFrame(columns = ["radius_mean","texture_mean","perimeter_mean","area_mean",
                                        "smoothness_mean","compactness_mean","concavity_mean","concave points_mean",
                                        "symmetry_mean","fractal_dimension_mean","radius_se","texture_se","perimeter_se",
                                        "area_se","smoothness_se","compactness_se","concavity_se","concave points_se","symmetry_se",
                                        "fractal_dimension_se","radius_worst","texture_worst","perimeter_worst","area_worst","smoothness_worst",
                                        "compactness_worst","concavity_worst","concave points_worst","symmetry_worst","fractal_dimension_worst"])
-    df_input.loc[0] = [1,16.13,20.68,108.1,798.8,0.117,0.2022,0.1722,0.1028,0.2164,0.07356,0.5692,1.073,3.854,54.18,0.007026,0.02501,0.03188,0.01297,0.01689,0.004142,20.96,31.48,136.8,1315,0.1789,0.4233,0.4784,0.2073,0.3706,0.1142]
+    df_input.loc[0] = [16.13,20.68,108.1,798.8,0.117,0.2022,0.1722,0.1028,0.2164,0.07356,0.5692,1.073,3.854,54.18,0.007026,0.02501,0.03188,0.01297,0.01689,0.004142,20.96,31.48,136.8,1315,0.1789,0.4233,0.4784,0.2073,0.3706,0.1142]
     #make prediction
     result = loaded_model.predict(df_input)
     #print(result)
--- breast_cancer_save.py
+++ breast_cancer_save.py
@@ -1,13 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 2020-11-18
-@author: Dr. Ganjar Alfian
-email : ganjar@dongguk.edu
-for teaching purpose only.
-"""
-
 import pandas as pd
-from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
@@ -23,14 +15,17 @@ if __name__ == '__main__':
     #read the dataset
     pd.options.display.max_columns=None
     df = pd.read_csv('data.csv')
-    df = df.drop(columns=['id','Unnamed: 32'],inplace=True)
+    df = df.drop(columns=['id','Unnamed: 32'])
+
     #get the X and y
-    df_X = df.drop(columns=['diagnosis'],axis=1)
+    df_X = df.drop(['diagnosis'],axis=1)
     df_y = df['diagnosis']
 
-
-
-     #menyimpan X dan y menjadi numpy arrays
+    #relabelling the class
+    df_y=df_y.replace('B',0)
+    df_y=df_y.replace('M',1)
+    
+    #menyimpan X dan y menjadi numpy arrays
     X = df_X.astype(float).values
     y = df_y.astype(float)
 
@@ -41,16 +36,12 @@ if __name__ == '__main__':
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
 
-    
     #get the numerical attributes
     #df_X_numb = df_X.drop(columns=['diagnosis'], axis=1)
     #convert single categorical column into numeric, label encoding    
     #le = preprocessing.LabelEncoder()
     #df_X['diagnosis'] = le.fit_transform(df_X['diagnosis'])
 
-    
-    #combine numerical attributes with a newly converted attribute (categorical to numeric, Sex column)
-    df_X_new = pd.concat([df_X['diagnosis'], df_y], axis=1)
     #generate the model 
     print('Generate Logistic Regression Model')
-    lr(df_X_new, df_y)    
+    lr(X_train, y_train)    
